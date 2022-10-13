93a6819db3ca9c01e5dbc928369c03fdc5c3ae8a
Aminuxe <115246185+Aminuxe@users.noreply.github.com>
2022-10-13 16:20:44 +0100

    Add files via upload

---
+++ ebank-service/src/main/java/org/sid/ebankservice/entites/BankAccount.java
@@ -0,0 +1,31 @@
+package org.sid.ebankservice.entites;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.ebankservice.enums.AccountType;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BankAccount {
+
+    @Id
+
+    private String id;
+    private Date createdAt;
+    private Double balance;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 15)
+    private AccountType type;
+    private String currency;
+
+
+}
+
